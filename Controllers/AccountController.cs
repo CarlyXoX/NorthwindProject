5b68b23f242c3baa4c8ddbb55a8551346a390d18
CarlyXoX <cjanscha@my.wctc.edu>
2025-11-22 17:44:18 -0600

    Require authorization for the site, while still allowing access to the login page itself

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,17 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
+//Should require authorization for account, while allowing the user to still access login page
+[Authorize]
 public class AccountController(UserManager<AppUser> userMgr, SignInManager<AppUser> signInMgr) : Controller
 {
     private readonly UserManager<AppUser> _userManager = userMgr;
     private readonly SignInManager<AppUser> _signInManager = signInMgr;
+
+    [AllowAnonymous]
     public IActionResult Login(string returnUrl)
     {
         // return url remembers the user's original request
         ViewBag.returnUrl = returnUrl;
         return View();
     }
-    [HttpPost, ValidateAntiForgeryToken]
+
+    //Allow secure access to login page
+    [HttpPost, AllowAnonymous, ValidateAntiForgeryToken]
     public async Task<IActionResult> Login(UserLogin details, string returnUrl)
     {
         if (ModelState.IsValid)
--- Models/UserLogin.cs
+++ Models/UserLogin.cs
@@ -1,5 +1,5 @@
 using System.ComponentModel.DataAnnotations;
-
+//Login is already required
 public class UserLogin
 {
     [Required, UIHint("email")]
